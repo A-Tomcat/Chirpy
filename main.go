7629e299bfa6a5df9ad5fbb2546e8551a468270e
A-Tomcat <tom@ollfisch.de>
2026-05-01 14:15:10 +0200

    Commit 1.

--- main.go
+++ main.go
@@ -1,1 +1,31 @@
 package main
+
+import "net/http"
+
+/*
+to build the Server:
+go build -o out && ./out
+*/
+
+func handler(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	writer.WriteHeader(http.StatusOK)
+	_, err := writer.Write([]byte("OK\n"))
+	if err != nil {
+		return
+	}
+}
+
+func main() {
+	const filepathRoot = "."
+	sMux := http.NewServeMux()
+	newServer := http.Server{
+		Handler: sMux,
+		Addr:    ":8080",
+	}
+	fServer := http.FileServer(http.Dir(filepathRoot))
+	sMux.Handle("/app/", http.StripPrefix("/app", fServer))
+	sMux.HandleFunc("/healthz", handler)
+
+	newServer.ListenAndServe()
+}
